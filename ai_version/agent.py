aba42ece93b8ecf7c69f90ed3468db377073959d
akos.peter <peterakos19@gmail.com>
2024-05-29 11:49:18 +0300

    feat: Deep Q Agent and model

---
+++ ai_version/agent.py
@@ -0,0 +1,84 @@
+import random
+
+from torch import optim, nn
+import torch
+
+from ai_version.model import ReplayBuffer, DQN
+
+
+class DQNAgent:
+    def __init__(self, state_dim, action_dim, hidden_dim=64, lr=1e-3, gamma=0.99, buffer_capacity=10000, batch_size=64,
+                 epsilon_start=1.0, epsilon_end=0.01, epsilon_decay=500, train=False):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.gamma = gamma
+        self.batch_size = batch_size
+
+        self.model = DQN(state_dim, action_dim, hidden_dim)
+        self.target_model = DQN(state_dim, action_dim, hidden_dim)
+        self.target_model.load_state_dict(self.model.state_dict())
+        self.target_model.eval()
+
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        self.loss_fn = nn.MSELoss()
+
+        self.replay_buffer = ReplayBuffer(buffer_capacity)
+        self.epsilon = epsilon_start
+        self.epsilon_end = epsilon_end
+        self.epsilon_decay = epsilon_decay
+        self.steps_done = 0
+        self.perform_training = train
+
+    def get_action(self, state):
+        self.steps_done += 1
+        if random.random() > self.epsilon:
+            with torch.no_grad():
+                state = torch.FloatTensor(state).unsqueeze(0)
+                q_values = self.model(state)
+                action = q_values.max(1)[1].item()
+        else:
+            action = random.randrange(self.action_dim)
+
+        self.epsilon = max(self.epsilon_end, self.epsilon - (1 / self.epsilon_decay))
+
+        if self.perform_training:
+            self.train()
+
+        if self.perform_training and self.steps_done % 1000 == 0:
+            self.update_target_model()
+        return action
+
+    def forward(self, state):
+        with torch.no_grad():
+            q_values = self.model(state)
+        return q_values
+
+    def train(self):
+        if len(self.replay_buffer) < self.batch_size:
+            return None
+
+        state, action, reward, next_state, done = self.replay_buffer.sample(self.batch_size)
+
+        state = torch.FloatTensor(state)
+        next_state = torch.FloatTensor(next_state)
+        action = torch.LongTensor(action)
+        reward = torch.FloatTensor(reward)
+        done = torch.FloatTensor(done)
+
+        q_values = self.model(state)
+        next_q_values = self.target_model(next_state)
+
+        q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+        next_q_value = next_q_values.max(1)[0]
+        expected_q_value = reward + (1 - done) * self.gamma * next_q_value
+
+        loss = self.loss_fn(q_value, expected_q_value.detach())
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item()
+
+    def update_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
---
+++ ai_version/model.py
@@ -0,0 +1,37 @@
+import random
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import deque
+
+
+class DQN(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim=64):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        q_values = self.fc3(x)
+        return q_values
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        state = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+        return np.concatenate(state), action, reward, np.concatenate(next_state), done
+
+    def __len__(self):
+        return len(self.buffer)
+
